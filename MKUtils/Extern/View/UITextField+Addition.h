81ae5172c4e2ed7a917422254dbe80bbbac6be04
zhengmiaokai <965934999@qq.com>
2021-10-09 11:25:12 +0800

    添加正则过滤逻辑

--- MKUtils/Extern/View/UITextField+Addition.h
+++ MKUtils/Extern/View/UITextField+Addition.h
@@ -1,16 +1,27 @@
 //
 //  UITextField+Addition.h
-//  LMBModuleTool
+//  Basic
 //
 //  Created by zhengmiaokai on 2017/5/23.
-//  Copyright © 2017年 辣妈帮. All rights reserved.
+//  Copyright © 2017年 zhengmiaokai. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
+typedef void (^LCTextChangedBlock)(NSString *text);
+
 @interface UITextField (Addition)
 
-@property (nonatomic) NSInteger maxLength;
+/// 文本变化的回调
+@property (nonatomic, copy) LCTextChangedBlock textChangedBlock;
+
+/// 限制文本长度
+@property (nonatomic, assign) NSInteger maxLength;
+
+/// 过滤指定字符
+@property (nonatomic, copy) NSString* regularPattern;
+
+- (void)removeEditObserver;
 
 - (void)showInputAccessoryView;
 
